f45fc446f7ddc10c6dd20a87f0e0a7f0744bed65
Ricardo Pinto <ricardocrp27@gmail.com>
2023-07-11 16:11:34 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/ricardorodrigues27/gopportunities.git/router"
+import (
+	"github.com/ricardorodrigues27/gopportunities.git/config"
+	"github.com/ricardorodrigues27/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
