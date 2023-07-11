f29a162bc196d4252300ba671b4a2ee00296070b
Ricardo Pinto <ricardocrp27@gmail.com>
2023-07-11 17:39:41 -0400

    feat: create response and request helpers

---
+++ handler/response.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(c *gin.Context, code int, msg string) {
+	c.Header("Content-type", "application/json")
+	c.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(c *gin.Context, op string, data interface{}) {
+	c.Header("Content-type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation %s successfull", op),
+		"data":    data,
+	})
+}
